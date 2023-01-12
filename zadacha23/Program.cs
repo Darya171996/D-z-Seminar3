0f16485ec35d54d1b25142fd87e3094193d4f00f
PetrikovaDarya <moskov.ru@mail.ru>
2023-01-12 14:25:49 +0400

    Задача23

---
+++ zadacha23/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+
+Console.WriteLine("Веедите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[N];
+for (int i = 0; i < N; i++)
+{
+    // array[i] = (i + 1) * (i + 1) * (i + 1);
+    array[i] = Math.Pow(i + 1, 3);
+    Console.WriteLine($"{array[i]}");
+}
+
